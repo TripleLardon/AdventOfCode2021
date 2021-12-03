8b7eb9956f9158d0540b18e057cfcf990365f654
TripleLardon <bardinet.andrea@gmail.com>
2021-12-03 22:36:06 +0100

    day1 **

--- day1/day1.py
+++ day1/day1.py
@@ -3,14 +3,24 @@
 
 input = open("input","r")
 lines = input.readlines()
+lines = list(map(lambda s: int(s[:-1]),lines))
 
-last = int(lines[0])
+last = lines[0]
 part1 = 0
 
 for line in lines[1:]:
-    line = int(line)
     if(line >  last):
         part1+=1
     last = line
 
-print("part1: ",part1)
+print("Part1:", part1)
+
+last = lines[:3]
+part2 = 0
+for line in lines[3:]:
+    if sum(last[1:])+line > sum(last):
+        part2+=1
+    last.append(line)
+    last.pop(0)
+
+print("Part2", part2)
